1f5e13ef8fd7d3e6baa618488d2c5b55790a39eb
Marta <lagiewka.marta.dev@gmail.com>
2019-06-10 20:47:02 +0200

    FINISH About section

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import {Section} from '../globals/Section';
-import Title from '../globals/Title';
+import React from "react";
+import { Section } from "../globals/Section";
+import Title from "../globals/Title";
+import aboutImg from "../../images/aboutBcg.jpeg";
+import {
+  setRem,
+  setBorder,
+  setColor,
+  setLetterSpacing,
+  media
+} from "../../styles";
+import { PrimaryBtn } from "../globals/Buttons";
+import styled from "styled-components";
 
 const About = () => {
-    return (
-        <Section color="pink">
-        <Title title="About us"></Title>
-        </Section>
-    )
+  return (
+    <Section color="pink">
+      <AboutCenter>
+        <div className="about-img">
+          <img src={aboutImg} alt="about us" />
+        </div>
+        <div className="about-info">
+        <Title title="About us" />
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <PrimaryBtn>read more</PrimaryBtn>
+        </div>
+      </AboutCenter>
+    </Section>
+  );
+};
+const AboutCenter = styled.div`
+.about-image-rendering, .about-info {
+    padding: ${setRem(30)};
 }
+.about-img {
+    img {
+        width: 100%;
+        display: block;
+        ${setBorder({width:setRem(6), color: setColor.primaryColor})}
+    }
+}
+.about-info {
+    p {
+      ${setLetterSpacing(3)}
 
-export default About
+    }
+}
+width: 90vw;
+margin: 0 auto;
+${media.desktop`
+width: 100vw;
+max-width: 1170px;
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-column-gap: ${setRem(32)};
+.about-img {
+    align-self: center;
+}
+.about-info{
+    p{
+        width: 80%;
+    }
+}
+`}
+
+
+`;
+export default About;
